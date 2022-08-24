51a81508195b5d223cafe8bffa3f5c684cd4480d
gowrigoutham <90804126+gowrigoutham@users.noreply.github.com>
2022-08-24 17:33:34 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/***************************
+ * Hello.java program
+ * *************************
+ * Gowri Goutham 
+ * Wednesday August 24
+ * CMSC 255
+ * ************************/
+ 
+
+ public class Hello {
+ 	public static void main(String[] args) {
+ 		System.out.println("Gowri Goutham");
+ 		System.out.println("Hello.java");
+ 		System.out.println("August 24, 2022");
+ 		System.out.println("cmsc255 & 004");
+ 		System.out.println();
+
+ 	}
+ }
